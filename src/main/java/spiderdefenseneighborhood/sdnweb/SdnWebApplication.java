f2a6ea765c86f4abcbe882411fc6dc121090cc92
leo_g <leo_g@DESKTOP-Q7T3S3B>
2020-11-03 22:25:21 -0300

    Start

---
+++ src/main/java/spiderdefenseneighborhood/sdnweb/SdnWebApplication.java
@@ -0,0 +1,13 @@
+package spiderdefenseneighborhood.sdnweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdnWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spiderdefenseneighborhood/sdnweb/SdnWebApplicationTests.java
@@ -0,0 +1,13 @@
+package spiderdefenseneighborhood.sdnweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdnWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
